501aaaadf956c39f795ab1b692c5f2350a5f0c83
Taislin <cariosecus@gmail.com>
2020-06-23 02:14:11 +0100

    less > css

--- client_src/gulpfile.js
+++ client_src/gulpfile.js
@@ -1,22 +1,18 @@
-
 var gulp = require("gulp");
 var sourcemaps = require("gulp-sourcemaps");
 var source = require("vinyl-source-stream");
 var buffer = require("vinyl-buffer");
 var browserify = require("browserify");
-var watchify = require("watchify");
-var less = require("gulp-less");
-var concat = require("gulp-concat");
 var log = require("gulplog");
 
-var bundler = browserify({
-	entries: "./index.js",
-	debug: true,
-	cache: {},
-	packageCache: {},
-});
 
-function bundle(used_bundler = bundler) {
+function bundle() {
+	var used_bundler = browserify({
+		entries: "./index.js",
+		debug: true,
+		cache: {},
+		packageCache: {},
+	});
 	return used_bundler
 		.bundle()
 		.on("error", function (err) {
@@ -30,27 +26,4 @@ function bundle(used_bundler = bundler) {
 		.pipe(gulp.dest("../res"));
 }
 
-gulp.task("js", function () {
-	return bundle();
-});
-
-gulp.task("css", function () {
-	return gulp
-		.src("./css/**/*.less")
-		.pipe(sourcemaps.init())
-		.pipe(less())
-		.pipe(concat("client.css"))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest("../res"));
-});
-
-gulp.task("watch", function () {
-	bundler.plugin(watchify);
-	gulp.series("js")();
-	bundler.on("update", gulp.series("js"));
-	return gulp.watch(["css/**/*.less"], gulp.series("css"));
-});
-
-gulp.task("default", gulp.parallel("js", "css"));
-
-gulp.task("all", gulp.series("css", "watch"));
+gulp.task("default", function () {return bundle();});
