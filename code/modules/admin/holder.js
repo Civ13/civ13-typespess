35d1b0f2bb45685c1fea4f582bffd8f42d772854
cariosecus <cariosecus@gmail.com>
2020-07-04 21:20:24 +0100

    admin lists

--- code/modules/admin/holder.js
+++ code/modules/admin/holder.js
@@ -1,3 +1,6 @@
+const read_config = require("../../config.js");
+
+const adminlist = read_config("admins.cson");
 class AdminHolder {
 	constructor(client) {
 		client.holder = this;
@@ -13,7 +16,7 @@ module.exports = AdminHolder;
 
 module.exports.now = (server) => {
 	server.on("client_login", (client) => {
-		if (client.address == "127.0.0.1" || client.address == "::1") {
+		if (client.address == "127.0.0.1" || client.address == "::1" || client.name in adminlist.hosts) {
 			client.holder = new AdminHolder(client);
 		}
 	});
