360392f70d50c0d8e50cd899da69d89415d7ff15
Taislin <cariosecus@gmail.com>
2020-07-10 02:04:13 +0100

    new scripts

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -9,24 +9,21 @@ cd..
 call npm install
 call npm install -g typescript
 call npm install -g ts-node
-
+call npm install -g pouchdb-server
 echo.
 echo Packages installed.
-
 cd client_src
 echo Browserifying client...
-
 echo.
 call ts-node compile.ts
-
 echo.
 echo Done.
-
 echo.
 echo Generating the map...
+cd..
 cd maps
 call ts-node mapgen.ts
 echo .
 echo Everything is done.
-
 echo.
+pause
