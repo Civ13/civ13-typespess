44c82db55c3876c8c0803e0ea230e1d106b8bb10
Taislin <cariosecus@gmail.com>
2021-01-24 02:33:59 +0000

    bodypart fixes

--- code/game/mobs/living/carbon/human/human_parts.js
+++ code/game/mobs/living/carbon/human/human_parts.js
@@ -6,14 +6,29 @@ module.exports.templates = {
 	human_r_arm: {
 		components: ["BodyPartSkinTone", "RArmBodyPart"],
 	},
+	human_l_hand: {
+		components: ["BodyPartSkinTone", "LHandBodyPart"],
+	},
+	human_r_hand: {
+		components: ["BodyPartSkinTone", "RHandBodyPart"],
+	},
 	human_l_leg: {
 		components: ["BodyPartSkinTone", "LLegBodyPart"],
 	},
 	human_r_leg: {
 		components: ["BodyPartSkinTone", "RLegBodyPart"],
 	},
+	human_l_foot: {
+		components: ["BodyPartSkinTone", "LFootBodyPart"],
+	},
+	human_r_foot: {
+		components: ["BodyPartSkinTone", "RFootBodyPart"],
+	},
 	human_torso: {
 		components: ["BodyPartSkinTone", "TorsoBodyPart"],
+	},
+	human_groin: {
+		components: ["BodyPartSkinTone", "GroinBodyPart"],
 	},
 	human_head: {
 		components: ["BodyPartSkinTone", "BodyPartHumanHair", "HeadBodyPart"],
