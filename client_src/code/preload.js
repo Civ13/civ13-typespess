170b5d8d88653327c6885c2224d7231eb361a8ae
Taislin <cariosecus@gmail.com>
2021-01-27 01:18:59 +0000

    disable preloading for now

--- client_src/code/preload.js
+++ client_src/code/preload.js
@@ -1,7 +1,7 @@
 const preload_list = require("./preloadlist.js");
 let tpreload_list = ["icons/error.png"];
-if (Array.isArray(preload_list) == true)
-	{tpreload_list = preload_list;}
+//if (Array.isArray(preload_list) == true)
+//	{tpreload_list = preload_list;}
 module.exports = async function preload(client) {
 	for (let path of tpreload_list) {
 		if (!client.icon_metas[path]) await client.enqueue_icon_meta_load(path);
