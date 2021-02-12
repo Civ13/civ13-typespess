d501cdf99478b7af01dd8fa78dd3c8e811b65b53
Taislin <cariosecus@gmail.com>
2021-02-12 01:07:04 +0000

    some atom fixes

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,32 +1,32 @@
 declare namespace NodeJS {
-    type Tserver = any;
-    type Tworld = any;
-    interface Global {
-        Tserver: Tserver,
-        Tworld: Tworld,
-        is_bs_editor_env: boolean,
-        workspaceDir: string,
-    }
+	type Tserver = any;
+	type Tworld = any;
+	interface Global {
+		Tserver: Tserver,
+		Tworld: Tworld,
+		is_bs_editor_env: boolean,
+		workspaceDir: string,
+	}
 }
 
 interface Element {
-    style: any,
-    tabIndex: any,
-    dataset: any,
-    focus: any,
-    getContext: any,
+	style: any,
+	tabIndex: any,
+	dataset: any,
+	focus: any,
+	getContext: any,
 }
 
 interface EventTarget {
-    localName: any,
-    id: any,
+	localName: any,
+	id: any,
 }
 
 interface HTMLElement {
-    value: any,
+	value: any,
 }
 
 interface ChildNode {
-    classList: any,
-    dataset: any,
+	classList: any,
+	dataset: any,
 }
